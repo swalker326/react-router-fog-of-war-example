d17427a20b4327ed0e79ec4da1328782c90f31c1
Shane <shane@swalker.dev>
2024-06-08 13:51:20 -0500

    updated route manifest

--- host/src/routeManifest.ts
+++ host/src/routeManifest.ts
@@ -1,4 +1,5 @@
 import { routes as RemoteOneRoutes } from "remote/routes";
 import { routes as RemoteTwoRoutes } from "remotetwo/routes";
+import { routes as DashboardRoutes } from "dashboard/routes";
 
-export const routeManifest = [...RemoteOneRoutes, ...RemoteTwoRoutes];
+export const routeManifest = [...RemoteOneRoutes, ...RemoteTwoRoutes, ...DashboardRoutes];
