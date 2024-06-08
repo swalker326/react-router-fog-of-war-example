4150428164853d4ed9ba4a492f23fb2764b3f0d1
Shane <shane@swalker.dev>
2024-06-08 13:51:57 -0500

    updated module types

--- host/src/types-module-federation.d.ts
+++ host/src/types-module-federation.d.ts
@@ -7,3 +7,7 @@ declare module "remotetwo/routes" {
   import { Route } from "react-router-dom";
   export const routes: Route[];
 }
+declare module "dashboard/routes" {
+  import {Route} from "react-router-dom";
+  export const routes: Route[];
+}
