ebc24244ded29fb707718bae78f5f46bd7b15c3e
Shane <shane@swalker.dev>
2024-06-09 10:03:39 -0500

    added tailwind to remote-two

--- remote-two/src/App.tsx
+++ remote-two/src/App.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import './index.css';
 
 const App = () => {
   return <Outlet />;
--- remote-two/src/routes/index.tsx
+++ remote-two/src/routes/index.tsx
@@ -2,7 +2,7 @@
 export default function IndexRoute() {
   return (
     <div>
-      <h1>Remote-Two</h1>
+      <h1 className="text-red-500">Remote-Two</h1>
       <p>Remote app mounted from localhost:3001</p>
     </div>
   );
